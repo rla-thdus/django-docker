cef9636f63af46966dcfac641a7c83c09e1fc968
rla_thdus <0228ksy@naver.com>
2022-10-02 00:04:13 +0900

    Update detail() question not exist raise http 404 error

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse, HttpRequest
+from django.http import HttpResponse, HttpRequest, Http404
 from django.shortcuts import render
 
 from .models import Question
@@ -9,7 +9,11 @@ def index(request: HttpRequest):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
